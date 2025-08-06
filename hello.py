91be0d98a82d866e00759a70581956ca28af98b9
DannyBashy98 <dannybashy98@gmail.com>
2025-08-06 17:48:57 -0400

    Update hello.py
    updated repo

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 # Danny Bashy  
-# 08/06/2025  
+# 08/06/2025
 # A simple Python program to print a message 
 
-print("Hello, World")
+# Danny Bashy
+# 08/06/2025
+# added a new message 
+
+print("Hello World!")
+
+print("Welcome to GitHub!") 
